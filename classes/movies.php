5c7b4a65d905bf12061075a5d95ec389116c4b7b
Luca Sammarco <luca.sammarco7@gmail.com>
2024-07-02 16:40:15 +0200

    added class Movie and added 3 movies to the class

--- classes/movies.php
+++ classes/movies.php
@@ -1,16 +1,24 @@
 <?php
 
 
-    class Movies {
-        
+    class Movie {
+        public $title;
+        public $director;
+        public $year;
+        public $genre;  
+    
+
+
+
+    public function __construct($title, $director, $year, $genre) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+        $this->genre = $genre;
     }
 
 
-
-
-
-
-
+    }
 
 
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require_once './classes/movies.php';
 
+$Godfather = new Movie('The Godfather', 'Francis Ford Coppola', 1972, 'Crime');
 
+$scarface = new Movie('Scarface', 'Brian De Palma', 1983, 'Crime');
 
+$goodfellas = new Movie('Goodfellas', 'Martin Scorsese', 1990, 'Crime');
+
+var_dump($Godfather, $scarface, $goodfellas);
 ?>
